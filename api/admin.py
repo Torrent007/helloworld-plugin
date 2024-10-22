c18a6e8a948a3994306fe1e60b31d89569e55b53
Fabien 'Torrent007' EGRET <fabien.egret@gmail.com>
2024-10-22 22:05:08 +0200

    update admin page

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at", "updated_at")
+    search_fields = ("name", "description")
+    list_filter = ("created_at", "updated_at")
